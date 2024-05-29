70d711d5a61053e3c9e04503e3bd8d0a86549679
zt <2916461184@qq.com>
2024-05-29 15:40:34 +0800

    add multi

---
+++ src/main/java/Multi.java
@@ -0,0 +1,14 @@
+/**
+ * @BelongsProject: idea_demo
+ * @BelongsPackage: PACKAGE_NAME
+ * @Author: zt
+ * @CreateTime: 2024-05-29  15:38
+ * @Description:
+ */
+
+public class Multi implements Computable{
+    @Override
+    public int compute(int num1, int num2) {
+        return num1 * num2;
+    }
+}
